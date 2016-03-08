5377f23e6a859518a3a11b9e9d90b29f55544b49
Josh Biol <joshbiol@fastmail.com>
2016-03-08 00:23:50 -0800

    Removed web-service/deployment.sh

--- web-service/deployment.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#Copy files to server
-rsync -avz --exclude "deployment.sh" -e "ssh -i /cygdrive/c/Users/joshb/.ssh/id_rsa -o StrictHostKeyChecking=no" --progress ./* josh@45.55.153.9:~/web-service
