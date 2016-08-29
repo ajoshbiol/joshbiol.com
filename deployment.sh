f57dc4d59bec7bc67f0cb9cfbc676ead60ccc4e7
Josh Biol <joshbiol@fastmail.com>
2016-08-28 23:14:11 -0700

    Removing unnecessary files in github.

--- deployment.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#Copy files to server
-rsync -avz --exclude "web-service/start.sh" --exclude "deployment.sh" --exclude "web-service/tools" --exclude "README" --progress ./* josh@45.55.153.9:~
